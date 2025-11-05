5396deddd5f4024a77501dd0c8dcc3d6172decf2
Vanechka-777 <vanya.chugunov2009@gmail.com>
2025-11-05 19:30:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import io
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
 
-template = '''<?xml version="1.0" encoding="UTF-8"?>
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QTableWidgetItem
+
+template = """<?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
+ <class>FuelCalculator</class>
+ <widget class="QMainWindow" name="FuelCalculator">
   <property name="geometry">
    <rect>
     <x>0</x>
@@ -23,225 +26,155 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
       <property name="currentIndex">
        <number>0</number>
       </property>
-      <widget class="QWidget" name="calc_tab">
+      <widget class="QWidget" name="tab_calculator">
        <attribute name="title">
         <string>Расчет расхода</string>
        </attribute>
        <layout class="QVBoxLayout" name="verticalLayout_2">
         <item>
-         <widget class="QLabel" name="label">
-          <property name="text">
-           <string>Расчет расхода топлива (л/100км)</string>
+         <widget class="QGroupBox" name="groupBox">
+          <property name="title">
+           <string>Параметры поездки</string>
           </property>
-          <property name="alignment">
-           <set>Qt::AlignCenter</set>
+          <layout class="QGridLayout" name="gridLayout">
+           <item row="0" column="0">
+            <widget class="QLabel" name="label">
+             <property name="text">
+              <string>Расстояние (км):</string>
+             </property>
+            </widget>
+           </item>
+           <item row="0" column="1">
+            <widget class="QLineEdit" name="distance_input">
+             <property name="text">
+              <string>100</string>
+             </property>
+            </widget>
+           </item>
+           <item row="1" column="0">
+            <widget class="QLabel" name="label_2">
+             <property name="text">
+              <string>Расход топлива (л/100км):</string>
+             </property>
+            </widget>
+           </item>
+           <item row="1" column="1">
+            <widget class="QLineEdit" name="consumption_input">
+             <property name="text">
+              <string>8.5</string>
+             </property>
+            </widget>
+           </item>
+           <item row="2" column="0">
+            <widget class="QLabel" name="label_3">
+             <property name="text">
+              <string>Цена топлива (руб/л):</string>
+             </property>
+            </widget>
+           </item>
+           <item row="2" column="1">
+            <widget class="QLineEdit" name="price_input">
+             <property name="text">
+              <string>50</string>
+             </property>
+            </widget>
+           </item>
+           <item row="3" column="0">
+            <widget class="QLabel" name="label_4">
+             <property name="text">
+              <string>Дата поездки:</string>
+             </property>
+            </widget>
+           </item>
+           <item row="3" column="1">
+            <widget class="QDateEdit" name="date_input"/>
+           </item>
+          </layout>
+         </widget>
+        </item>
+        <item>
+         <widget class="QGroupBox" name="groupBox_2">
+          <property name="title">
+           <string>Результаты расчета</string>
           </property>
+          <layout class="QGridLayout" name="gridLayout_2">
+           <item row="0" column="0">
+            <widget class="QLabel" name="label_5">
+             <property name="text">
+              <string>Топливо всего (л):</string>
+             </property>
+            </widget>
+           </item>
+           <item row="0" column="1">
+            <widget class="QLabel" name="fuel_total_label">
+             <property name="text">
+              <string>0.0</string>
+             </property>
+            </widget>
+           </item>
+           <item row="1" column="0">
+            <widget class="QLabel" name="label_7">
+             <property name="text">
+              <string>Стоимость (руб):</string>
+             </property>
+            </widget>
+           </item>
+           <item row="1" column="1">
+            <widget class="QLabel" name="cost_label">
+             <property name="text">
+              <string>0.0</string>
+             </property>
+            </widget>
+           </item>
+           <item row="2" column="0">
+            <widget class="QLabel" name="label_9">
+             <property name="text">
+              <string>На 1 км (руб):</string>
+             </property>
+            </widget>
+           </item>
+           <item row="2" column="1">
+            <widget class="QLabel" name="cost_per_km_label">
+             <property name="text">
+              <string>0.0</string>
+             </property>
+            </widget>
+           </item>
+          </layout>
          </widget>
         </item>
         <item>
          <layout class="QHBoxLayout" name="horizontalLayout">
           <item>
-           <widget class="QLabel" name="label_2">
+           <widget class="QPushButton" name="calculate_btn">
             <property name="text">
-             <string>Пройденное расстояние (км):</string>
+             <string>Рассчитать</string>
             </property>
            </widget>
           </item>
           <item>
-           <widget class="QLineEdit" name="distance_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_2">
-          <item>
-           <widget class="QLabel" name="label_3">
+           <widget class="QPushButton" name="save_btn">
             <property name="text">
-             <string>Потрачено топлива (л):</string>
+             <string>Сохранить запись</string>
             </property>
            </widget>
           </item>
           <item>
-           <widget class="QLineEdit" name="fuel_input"/>
+           <widget class="QPushButton" name="clear_btn">
+            <property name="text">
+             <string>Очистить</string>
+            </property>
+           </widget>
           </item>
          </layout>
-        </item>
-        <item>
-         <widget class="QPushButton" name="calculate_btn">
-          <property name="text">
-           <string>Рассчитать расход</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QLabel" name="result_label">
-          <property name="text">
-           <string>Результат:</string>
-          </property>
-          <property name="alignment">
-           <set>Qt::AlignCenter</set>
-          </property>
-         </widget>
         </item>
        </layout>
       </widget>
-      <widget class="QWidget" name="cost_tab">
+      <widget class="QWidget" name="tab_history">
        <attribute name="title">
-        <string>Стоимость поездки</string>
+        <string>История расчетов</string>
        </attribute>
        <layout class="QVBoxLayout" name="verticalLayout_3">
-        <item>
-         <widget class="QLabel" name="label_4">
-          <property name="text">
-           <string>Расчет стоимости поездки</string>
-          </property>
-          <property name="alignment">
-           <set>Qt::AlignCenter</set>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_3">
-          <item>
-           <widget class="QLabel" name="label_5">
-            <property name="text">
-             <string>Расстояние (км):</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QLineEdit" name="cost_distance_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_4">
-          <item>
-           <widget class="QLabel" name="label_6">
-            <property name="text">
-             <string>Расход (л/100км):</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QLineEdit" name="consumption_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_5">
-          <item>
-           <widget class="QLabel" name="label_7">
-            <property name="text">
-             <string>Цена топлива (руб/л):</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QLineEdit" name="price_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <widget class="QPushButton" name="calculate_cost_btn">
-          <property name="text">
-           <string>Рассчитать стоимость</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QLabel" name="cost_result_label">
-          <property name="text">
-           <string>Результат:</string>
-          </property>
-          <property name="alignment">
-           <set>Qt::AlignCenter</set>
-          </property>
-         </widget>
-        </item>
-       </layout>
-      </widget>
-      <widget class="QWidget" name="refuel_tab">
-       <attribute name="title">
-        <string>Добавить заправку</string>
-       </attribute>
-       <layout class="QVBoxLayout" name="verticalLayout_4">
-        <item>
-         <widget class="QLabel" name="label_8">
-          <property name="text">
-           <string>Добавление данных о заправке</string>
-          </property>
-          <property name="alignment">
-           <set>Qt::AlignCenter</set>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_6">
-          <item>
-           <widget class="QLabel" name="label_9">
-            <property name="text">
-             <string>Пройденное расстояние (км):</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QLineEdit" name="refuel_distance_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_7">
-          <item>
-           <widget class="QLabel" name="label_10">
-            <property name="text">
-             <string>Количество топлива (л):</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QLineEdit" name="refuel_fuel_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <layout class="QHBoxLayout" name="horizontalLayout_8">
-          <item>
-           <widget class="QLabel" name="label_11">
-            <property name="text">
-             <string>Цена за литр (руб):</string>
-            </property>
-           </widget>
-          </item>
-          <item>
-           <widget class="QLineEdit" name="refuel_price_input"/>
-          </item>
-         </layout>
-        </item>
-        <item>
-         <widget class="QPushButton" name="add_refuel_btn">
-          <property name="text">
-           <string>Добавить заправку</string>
-          </property>
-         </widget>
-        </item>
-       </layout>
-      </widget>
-      <widget class="QWidget" name="history_tab">
-       <attribute name="title">
-        <string>История</string>
-       </attribute>
-       <layout class="QVBoxLayout" name="verticalLayout_5">
-        <item>
-         <widget class="QLabel" name="label_12">
-          <property name="text">
-           <string>Последние 10 заправок</string>
-          </property>
-          <property name="alignment">
-           <set>Qt::AlignCenter</set>
-          </property>
-         </widget>
-        </item>
         <item>
          <widget class="QTableWidget" name="history_table">
           <column>
@@ -256,7 +189,7 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
           </column>
           <column>
            <property name="text">
-            <string>Топливо</string>
+            <string>Расход</string>
            </property>
           </column>
           <column>
@@ -266,12 +199,31 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
           </column>
           <column>
            <property name="text">
-            <string>Расход</string>
+            <string>Стоимость</string>
            </property>
           </column>
          </widget>
         </item>
         <item>
+         <layout class="QHBoxLayout" name="horizontalLayout_2">
+          <item>
+           <widget class="QPushButton" name="delete_btn">
+            <property name="text">
+             <string>Удалить запись</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="clear_history_btn">
+            <property name="text">
+             <string>Очистить историю</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="stats_btn">
+            <property name="text">
+             <string>Статистика</string>
             </property>
            </widget>
           </item>
@@ -279,36 +231,103 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
         </item>
        </layout>
       </widget>
+      <widget class="QWidget" name="tab_statistics">
+       <attribute name="title">
+        <string>Статистика</string>
+       </attribute>
+       <layout class="QVBoxLayout" name="verticalLayout_4">
+        <item>
+         <widget class="QLabel" name="stats_label">
+          <property name="text">
+           <string>Статистика будет отображена здесь после расчетов</string>
+          </property>
+         </widget>
+        </item>
+        <item>
+         <widget class="QTextEdit" name="stats_text" readonly="true"/>
+        </item>
+       </layout>
+      </widget>
      </widget>
     </item>
    </layout>
   </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>800</width>
+     <height>28</height>
+    </rect>
+   </property>
+   <widget class="QMenu" name="menu">
+    <property name="title">
+     <string>Файл</string>
+    </property>
+    <addaction name="action_export"/>
+    <addaction name="separator"/>
+    <addaction name="action_exit"/>
+   </widget>
+   <widget class="QMenu" name="menu_2">
+    <property name="title">
+     <string>Справка</string>
+    </property>
+    <addaction name="action_about"/>
+   </widget>
+   <addaction name="menu"/>
+   <addaction name="menu_2"/>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+  <action name="action_exit">
+   <property name="text">
+    <string>Выход</string>
+   </property>
+  </action>
+  <action name="action_about">
+   <property name="text">
+    <string>О программе</string>
+   </property>
+  </action>
+  <action name="action_export">
+   <property name="text">
+    <string>Экспорт данных</string>
+   </property>
+  </action>
  </widget>
  <resources/>
  <connections/>
 </ui>
-'''
+"""
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class FuelCalculatorApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main_window.ui", self)
+        self.history_data = []
 
-        self.bunon()
+        ui_file = io.StringIO(template)
+        uic.loadUi(ui_file, self)
 
+        self.setup_validators()
+        self.setup_connections()
+        self.setup_initial_state()
 
-    def buton(self):
-        self.setWindowTitle("Калькулятор расхода топлива")
-        self.calculate_btn.clicked.connect(self.calculate_consumption)
-        self.calculate_cost_btn.clicked.connect(self.calculate_cost)
-        self.add_refuel_btn.clicked.connect(self.add_refueling)
-        self.history_btn.clicked.connect(self.show_history_window)
-        self.stats_btn.clicked.connect(self.show_stats_window)
+    def setup_validators(self):
+        double_validator = QDoubleValidator(0.01, 10000.0, 2)
+        double_validator.setNotation(QDoubleValidator.Notation.StandardNotation)
+
+        price_validator = QDoubleValidator(0.01, 1000.0, 2)
+        price_validator.setNotation(QDoubleValidator.Notation.StandardNotation)
+
+        self.distance_input.setValidator(double_validator)
+        self.consumption_input.setValidator(double_validator)
+        self.price_input.setValidator(price_validator)
+
+    def setup_connections(self):
+        self.calculate_btn.clicked.connect(self.calculate_fuel)
+        self.save_btn.clicked.connect(self.save_calculation)
+        self.clear_btn.clicked.connect(self.clear_inputs)
+        self.delete_btn.clicked.connect(self.delete_history_entry)
         self.clear_history_btn.clicked.connect(self.clear_history)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MainWindow
-    ex.show()
-    sys.exit(app.exec())
+        self.stats_btn.clicked.connect(self.show_statistics)
